c0c59600d9c01f8901406ca4d47a63505eb5899f
shad0w46 <rutrupala.ce@gmail.com>
2026-07-18 07:44:49 +0530

    Assignment 4 part-1 complete

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -7,21 +7,15 @@
  * It should be returned by your thread so it can be freed by
  * the joiner thread.
  */
-struct thread_data{
-    /*
-     * TODO: add other values your thread will need to manage
-     * into this structure, use this structure to communicate
-     * between the start_thread_obtaining_mutex function and
-     * your thread implementation.
-     */
+struct thread_data {
 
-    /**
-     * Set to true if the thread completed with success, false
-     * if an error occurred.
-     */
+    pthread_mutex_t *mutex;
+
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
+
     bool thread_complete_success;
 };
-
 
 /**
 * Start a thread which sleeps @param wait_to_obtain_ms number of milliseconds, then obtains the
